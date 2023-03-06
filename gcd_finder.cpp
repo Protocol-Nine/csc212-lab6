5acafbb90a85c95d96636b91f0a1472d37447e06
Erik <proto@DESKTOP-O89RR9M>
2023-03-06 14:45:40 -0500

    permutations Heap's Algorithm

--- gcd_finder.cpp
+++ gcd_finder.cpp
@@ -18,29 +18,6 @@ int main(int argc, char*argv[]) {
     std::cout << gcd_finder(a,b) << std::endl;
 }
 
-// int gcd_finder(int a, int b, int gcd) {
-//     // Trivial cases of a = 0, b = 0, a = b
-//     if (a == 0 || a == b) {
-//         return b;
-//     }
-//     if (b == 0) {
-//         return a;
-//     }
-
-//     if ((gcd == 0) && (a > b)) {
-//         gcd = b;
-//     } else if ((gcd == 0) && (b > a)) {
-//         gcd = a;
-//     }
-
-//     if (a % gcd == 0 && b % gcd == 0) {
-//         return gcd;
-//     }
-
-//     return gcd_finder(a,b,gcd - 1);
-
-// }
-
 // Returns the gcd of two integer, assuming a is greater than or equal to b
 int gcd_finder(int a, int b) {
     // Trivial cases of a or b = 0, or a = b
--- permutations.cpp
+++ permutations.cpp
@@ -1,5 +1,53 @@
 #include <iostream>
+#include <vector>
+
+// Outputs all permutations of an array of ints.
+void permutations(std::vector<int> a, int current_permutation);
+// Prints the contents of a vector to terminal.
+void print_vector(std::vector<int> a);
 
 int main(int argc, char*argv[]) {
+    // CLA "n"
+    int n = std::stoi(argv[1]);
+
+    // Create a vector containing 1 through n
+    std::vector<int> n_vector;
+
+    for (int i = 1; i <= n; i++) {
+        n_vector.push_back(i);
+    }
+
+    permutations(n_vector, n_vector.size());
     
+}
+
+
+// Heap's Algorithm
+void permutations(std::vector<int> a, int current_permutation) {
+    // if current_permutation reaches 1, print array and return
+    if (current_permutation == 1) {
+        print_vector(a);
+        return;
+    }
+
+    for (int i = 0; i < current_permutation; i ++) {
+        permutations(a, current_permutation - 1);
+
+        // for even permutations
+            //swap i and last free element
+        if (current_permutation / 2 == 0) {
+            std::swap(a[i], a[current_permutation - 1]);
+        } else {
+        // for off permutations
+            // swap first and last free elements
+            std::swap(a[0], a[current_permutation - 1]);
+        }
+    }
+}
+
+void print_vector(std::vector<int> a) {
+    for (std::vector<int>::const_iterator it = a.begin(); it != a.end(); ++it) {
+        std::cout << *it << ' ';
+    }
+    std::cout << std::endl;
 }
