dd7b0ebd4f71da8d0cd5425691879df952c18303
Erik <proto@DESKTOP-O89RR9M>
2023-03-06 16:03:13 -0500

    Fixed Heap's Algorithm

--- permutations.cpp
+++ permutations.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 
 // Outputs all permutations of an array of ints.
-void permutations(std::vector<int> a, int current_permutation);
+void permutations(std::vector<int> &a, int current_permutation);
 // Prints the contents of a vector to terminal.
 void print_vector(std::vector<int> a);
 
@@ -23,24 +23,24 @@ int main(int argc, char*argv[]) {
 
 
 // Heap's Algorithm
-void permutations(std::vector<int> a, int current_permutation) {
+void permutations(std::vector<int> &a, int current_permutation) {
     // if current_permutation reaches 1, print array and return
     if (current_permutation == 1) {
         print_vector(a);
         return;
     }
 
-    for (int i = 0; i < current_permutation; i ++) {
+    for (int i = 0; i < current_permutation; i++) {
         permutations(a, current_permutation - 1);
 
-        // for even permutations
-            //swap i and last free element
-        if (current_permutation / 2 == 0) {
-            std::swap(a[i], a[current_permutation - 1]);
-        } else {
-        // for off permutations
-            // swap first and last free elements
+        // for odd permutations
+            //swap first and last free element
+        if (current_permutation % 2 == 1) {
             std::swap(a[0], a[current_permutation - 1]);
+        } else {
+        // for odd permutations
+            // swap i and last free elements
+            std::swap(a[i], a[current_permutation - 1]);
         }
     }
 }
